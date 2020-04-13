9b7fd169f7409f0fda322974245bc23e5aca472c
Kanika Sharma <kanica@Kanikas-MacBook-Pro.local>
2020-04-12 20:51:38 -0400

    user schema, routes and encryption

---
+++ controllers/user.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user");
+
+//load product model
+const productModel = require("../models/product");
+
+//Route for the Login
+router.get("/login", (req, res) => {
+  res.render("users/login", {
+    title: "ezSHOP | Login",
+  });
+});
+
+router.post("/login", (req, res) => {
+  const errorMessages = [];
+
+  //Validation
+  if (req.body.uname == "") {
+    errorMessages.push("You must enter a username");
+  }
+
+  if (req.body.psw == "") {
+    errorMessages.push("You must enter a password");
+  }
+
+  //No Errors
+  if (errorMessages.length == 0) {
+    const { uname, psw } = req.body; //destructuring
+
+    res.render("general/home", {
+      title: "ezSHOP",
+      bestSellers: productModel.getBestSellingProducts(),
+      message: `Welcome ${uname}!`,
+    });
+  }
+  //Errors
+  else {
+    res.render("users/login", {
+      title: "ezSHOP | Login",
+      errors: errorMessages,
+    });
+  }
+});
+
+//Route for the Sign-up
+router.get("/sign-up", (req, res) => {
+  res.render("users/sign-up", {
+    title: "ezSHOP | Sign Up",
+  });
+});
+
+//Route to process user's request and data when user submits registration form
+router.post("/sign-up", (req, res) => {
+/*
+    RULES when inserting: 
+    - create an instance
+    - pass the data submitted throrough the form in the form of an object
+    - call the save method
+*/
+  const newUser = 
+  {
+      firstName:req.body.firstName,
+      lastName:req.body.lastName,
+      email:req.body.email,
+      password:req.body.password
+  }
+// Create instance of the user model 
+  const user = new userModel(newUser);
+  user.save()
+  .then(()=>{
+    // res.redirect("general/home", {
+    //   title: "ezSHOP",
+    //   bestSellers: productModel.getBestSellingProducts(),
+    //   message: `Registration succesful. Hello ${firstName}, Welcome to ezSHOP!`,
+    // });
+
+    res.render("./general/home");
+
+  })
+  .catch(err=>console.log(`Error while inserting into the data ${err}`));
+
+
+/****** ERROR VALIDATION *****/
+  const errorMessages = [];
+  const { firstName, lastName, email, password } = req.body; //destructuring
+
+  //Null validation
+  if (firstName == "") {
+    errorMessages.push("You must enter a first name");
+  }
+  if (lastName == "") {
+    errorMessages.push("You must enter a last name");
+  }
+
+  if (email == "") {
+    errorMessages.push("You must enter an email");
+  }
+
+  if (password == "") {
+    errorMessages.push("You must enter a password");
+  }
+  // Password length check
+  const lengthRegex = /^.{6,12}$/;
+  if (lengthRegex.test(password) == false) {
+    errorMessages.push("Password must be between 6 to 12 characters");
+  }
+
+  // Password char check
+  const charRegex = /^[a-zA-Z0-9_]+$/;
+  if (charRegex.test(password) == false) {
+    errorMessages.push("Password must only contain letters and numbers");
+  }
+
+  //No Errors
+  if (errorMessages.length == 0) {
+    //sending email
+    const sgMail = require("@sendgrid/mail");
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+    const msg = {
+      to: `${email}`,
+      from: `kanika-k@hotmail.com`,
+      subject: "Registration Completed",
+      html: `Vistor's Full Name ${firstName} ${lastName} <br>
+     Vistor's Email Address ${email} <br>
+    `,
+    };
+
+    //Asynchornous operation (who don't know how long this will take to execute)
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.render("general/home", {
+          title: "ezSHOP",
+          bestSellers: productModel.getBestSellingProducts(),
+          message: `Registration succesful. Hello ${firstName}, Welcome to ezSHOP!`,
+        });
+      })
+      .catch((err) => {
+        console.log(`Error ${err}`);
+      });
+  }
+  //Errors
+  else {
+    res.render("users/sign-up", {
+      title: "ezSHOP | Sign Up",
+      name: firstName,
+      email: email,
+      password: password,
+      errors: errorMessages,
+    });
+  }
+});
+
+module.exports = router;
---
+++ models/user.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs"); //only using bcrypt in this file to encrypt passwords
+
+//This indicates the shape of the documents that will be entering the database
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+// "pre save" - this function executes right before the save method
+//  behavior of pre is akin to a middleware funtcion
+//  since it operates like a middleware, it requires next
+userSchema.pre("save", function (next) {
+  //salt random generated characters or strings
+  bcrypt
+    .genSalt(10) // generates 10 characters
+    .then((salt) => {
+      bcrypt
+        .hash(this.password, salt)
+        .then((encryptPassword) => {
+          this.password = encryptPassword;
+          next(); //commits the save and update the password
+        })
+        .catch((err) => console.log(`Error occured when hashing ${err}`));
+    })
+    .catch((err) => console.log(`Error occured when salting ${err}`));
+});
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
+
+/*
+    How do we encrupt the password?
+        - bcrypt uses double hashing
+        - uses salt that generates random texts 
+        - random text generated through salt gets concatenated with your password
+    What is salt?
+        - randomly generated characters or strings
+  */
