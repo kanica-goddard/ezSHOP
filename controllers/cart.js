d6020e361ed0966f0b4104e9808d4b551bb6fd3a
Kanika Sharma <kanica@Kanikas-MacBook-Pro.local>
2020-04-18 19:53:43 -0400

    Added UI for cart and get / delete in controller

--- controllers/cart.js
+++ controllers/cart.js
@@ -13,7 +13,36 @@ const productModel = require("../models/product"); //load product model
 
 //TODO: Place order + send email to logged-in user, indicating: all products in cart, quantity amount, total
 
-router.get("/add", isAuthenticated, (req, res) => {});
+router.get("/", isAuthenticated, (req, res) => {
+  cartModel.find().then((cartItems) => {
+    const mappedCartItems = cartItems.map((cartItem) => {
+      return {
+        productID: cartItem.productID,
+        productName: cartItem.productName,
+        price: cartItem.price,
+        quantity: cartItem.quantity,
+        productImage: cartItem.productImage,
+      };
+    });
+
+    res.render("cart/cart", {
+      title: "ezSHOP | Cart",
+      data: mappedCartItems,
+    });
+  });
+});
+
+router.delete("/delete/:id", (req, res) => {
+  console.log("Deleting");
+  console.log(req.params);
+
+  cartModel
+    .deleteOne({ productID: req.params.id })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(`Error occurred: ${err}`));
+});
 
 router.post("/add", isAuthenticated, (req, res) => {
   // find if product is already in the cart
@@ -27,7 +56,7 @@ router.post("/add", isAuthenticated, (req, res) => {
         //TODO: check amount in stock
 
         cartItem.quantity = newQuantity;
-        cartItem.save();
+        cartItem.save().then();
       } else {
         console.log("Get product info and create new cart item");
 
