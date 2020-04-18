3da29413b479f01b75d6b30d9dcd47b41a41ac43
Kanika Sharma <kanica@Kanikas-MacBook-Pro.local>
2020-04-17 20:56:24 -0400

    added real data to home page

--- controllers/general.js
+++ controllers/general.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
 //load product model
@@ -6,10 +6,30 @@ const productModel = require("../models/product");
 
 //Route for the Home Page
 router.get("/", (req, res) => {
-  res.render("general/home", {
-    title: "ezSHOP",
-    bestSellers: productModel.getBestSellingProducts()
-  });
+  productModel
+    .find({ isBestSeller: true })
+    .then((bestSellers) => {
+      const mappedBestSellers = bestSellers.map((product) => {
+        return {
+          id: product._id,
+          productImage: product.productImage,
+          description: product.description,
+          quantity: product.quantity,
+          productImage: product.productImage,
+          category: product.category,
+          price: product.price,
+          isBestSeller: product.isBestSeller,
+        };
+      });
+
+      res.render("general/home", {
+        title: "ezSHOP",
+        bestSellers: mappedBestSellers,
+      });
+    })
+    .catch((err) =>
+      console.log(`Error occured when pulling from the database :${err}`)
+    );
 });
 
 module.exports = router;
