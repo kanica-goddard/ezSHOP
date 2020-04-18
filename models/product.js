1b2da64c45ac616f3eaba8c2454aeb1070add9dc
Kanika Sharma <kanica@Kanikas-MacBook-Pro.local>
2020-04-17 21:01:45 -0400

    replaced fake data with product schema and model

--- models/product.js
+++ models/product.js
@@ -1,60 +1,50 @@
-const products = {
-  fakeDB: [],
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-  init() {
-    this.fakeDB.push({
-      title: "Acer Aspire 7 Ultra Slim 15.6",
-      price: `CDN$ 1,499.99`,
-      imgSrc: `prod1.jpg`,
-      isBestSeller: true
-    });
-
-    this.fakeDB.push({
-      title: "Canon EOS M100 EF-M 15-45mm",
-      price: `CDN$ 376.22`,
-      imgSrc: `prod2.jpg`,
-      isBestSeller: false
-    });
-
-    this.fakeDB.push({
-      title: "Kobo Clara HD-6 E-Reader",
-      price: `CDN$ 139.95`,
-      imgSrc: `prod3.jpg`,
-      isBestSeller: true
-    });
-
-    this.fakeDB.push({
-      title: "Fast Wireless Charging Stand",
-      price: `CDN$ 25.49`,
-      imgSrc: `prod4.jpg`,
-      isBestSeller: true
-    });
-
-    this.fakeDB.push({
-      title: "Bluetooth AII0 5.0 Earbuds",
-      price: `CDN$ 24.99`,
-      imgSrc: `prod5.jpg`,
-      isBestSeller: false
-    });
-
-    this.fakeDB.push({
-      title: "Waterproof Activity Tracker",
-      price: `CDN$ 36.99`,
-      imgSrc: `prod6.jpg`,
-      isBestSeller: true
-    });
+//This indicates the shape of the documents that will be entering the database
+const productSchema = new Schema({
+  productName: {
+    type: String,
+    required: true,
   },
 
-  getAllProducts() {
-    return this.fakeDB;
+  price: {
+    type: Number,
+    required: true,
   },
 
-  getBestSellingProducts() {
-    return this.fakeDB.filter(product => {
-      return product.isBestSeller == true;
-    });
-  }
-};
+  description: {
+    type: String,
+    required: true,
+  },
 
-products.init();
-module.exports = products;
+  category: {
+    type: String,
+    required: true,
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+  },
+
+  isBestSeller: {
+    type: Boolean,
+    required: true,
+  },
+
+  productImage: {
+    type: String,
+    required: true,
+  },
+
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+//Implementing this model allows us to perform CRUD operations
+const productModel = mongoose.model("Products", productSchema);
+
+module.exports = productModel;
