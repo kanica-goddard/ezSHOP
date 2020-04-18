492a1da54f973628f04c128d8ceba4628f001a92
Kanika Sharma <kanica@Kanikas-MacBook-Pro.local>
2020-04-17 22:29:30 -0400

    flattening query menthod to request

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
+app.use((req,res,next)=>{
+
+  if(req.query.method=="PUT")
+  {
+      req.method="PUT"
+  }
+
+  else if(req.query.method=="DELETE")
+  {
+      req.method="DELETE"
+  }
+
+  next();
+})
+
 app.use(fileUpload());
 
 app.use(session({
