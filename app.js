22efa3c30a02b0e91bccb8d6cec762df57d72a41
Kanika Sharma <kanica@Kanikas-MacBook-Pro.local>
2020-04-18 19:52:24 -0400

    Formatting

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express"); //this imports the express package that was installed within your application
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const session = require('express-session');
-const fileUpload = require('express-fileupload');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const fileUpload = require("express-fileupload");
 
 //load the environment variable file
 require("dotenv").config({ path: "./config/keys.env" });
@@ -19,34 +19,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
-app.use((req,res,next)=>{
-
-  if(req.query.method=="PUT")
-  {
-      req.method="PUT"
-  }
-
-  else if(req.query.method=="DELETE")
-  {
-      req.method="DELETE"
+app.use((req, res, next) => {
+  if (req.query.method == "PUT") {
+    req.method = "PUT";
+  } else if (req.query.method == "DELETE") {
+    req.method = "DELETE";
   }
 
   next();
-})
+});
 
 app.use(fileUpload());
 
-app.use(session({
-  secret: `${process.env.SECRET_KEY}`,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: `${process.env.SECRET_KEY}`,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   //a global variable that can be handled by any handlebar file
-  res.locals.user= req.session.userInfo;
+  res.locals.user = req.session.userInfo;
   next();
-})
+});
 
 //load controllers
 const generalController = require("./controllers/general");
@@ -60,12 +57,18 @@ app.use("/product", productController);
 app.use("/user", userController);
 app.use("/cart", cartController);
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{
-    console.log("Connected to MongoDB Database")
-})
-.catch(err=>console.log(`Error occured when connecting to database ${err}`));
-mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Database");
+  })
+  .catch((err) =>
+    console.log(`Error occured when connecting to database ${err}`)
+  );
+mongoose.set("useCreateIndex", true);
 
 //Sets up server - Creates an Express Web Server that listens to HTTP Reuqest on port 3000
 app.listen(process.env.PORT, () => {
